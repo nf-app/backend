e11c1cbc1ccdf9da291103c331df531b80513d33
ptylczynski <piotr@ptl.cloud>
2022-12-25 15:18:25 +0100

    init2

---
+++ api/src/test/java/cloud/ptl/api/ApiApplicationTests.java
@@ -0,0 +1,8 @@
+package cloud.ptl.api;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+}
---
+++ harvesters/interia-harvester/src/main/java/cloud/ptl/interiaharvester/InteriaHarvesterApplication.java
@@ -0,0 +1,12 @@
+package cloud.ptl.interiaharvester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteriaHarvesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InteriaHarvesterApplication.class, args);
+    }
+}
