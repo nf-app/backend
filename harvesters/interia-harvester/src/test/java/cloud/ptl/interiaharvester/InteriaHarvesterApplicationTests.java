eca4f27731394b8033b4569b30e2d4540333fa6c
ptylczynski <piotr@ptl.cloud>
2022-12-25 15:18:31 +0100

    init2

---
+++ harvesters/interia-harvester/src/test/java/cloud/ptl/interiaharvester/InteriaHarvesterApplicationTests.java
@@ -0,0 +1,12 @@
+package cloud.ptl.interiaharvester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteriaHarvesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
