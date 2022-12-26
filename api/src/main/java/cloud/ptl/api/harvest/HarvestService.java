0b10679908c591516f574591201074512d13c30a
ptylczynski <piotr@ptl.cloud>
2022-12-26 14:20:59 +0100

    add: HarvestService

--- api/src/main/java/cloud/ptl/api/harvest/HarvestService.java
+++ api/src/main/java/cloud/ptl/api/harvest/HarvestService.java
@@ -1,5 +1,24 @@
 package cloud.ptl.api.harvest;
 
-public class HarvestService {
+import cloud.ptl.harvest.HarvestSourceType;
+import cloud.ptl.kafka.message.HarvestRequestMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
+@Component
+public class HarvestService {
+    @Value("${kafka.order-topic}")
+    private String orderTopic;
+    private final KafkaTemplate<String, HarvestRequestMessage> kafkaTemplate;
+
+    public void startHarvest(HarvestSourceType harvestSourceType) {
+        kafkaTemplate.send(orderTopic, createRequestMessage(harvestSourceType));
+    }
+
+    private HarvestRequestMessage createRequestMessage(HarvestSourceType harvestSourceType) {
+        return new HarvestRequestMessage(harvestSourceType);
+    }
 }
